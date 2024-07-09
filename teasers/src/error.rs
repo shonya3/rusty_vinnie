92026c7567addbf7db691ba569af4a988ccbac8a
shonya3 <poeshonya3@gmail.com>
2024-07-09 14:39:11 +0300

    impl display for error

--- teasers/src/error.rs
+++ teasers/src/error.rs
@@ -1,9 +1,20 @@
+use std::fmt::Display;
+
 use crate::ParseTeasersThreadError;
 
 #[derive(Debug)]
 pub enum Error {
     ReqwestError(reqwest::Error),
     ParseTeasersThread(ParseTeasersThreadError),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::ReqwestError(err) => err.fmt(f),
+            Error::ParseTeasersThread(err) => err.fmt(f),
+        }
+    }
 }
 
 impl From<ParseTeasersThreadError> for Error {
