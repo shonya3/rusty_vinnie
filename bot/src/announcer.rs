f3c5c800f51bb3b4b73ee58f7c166b4d2d6ed094
shonya3 <poeshonya3@gmail.com>
2026-05-31 22:17:03 +0300

    create private league_start module for unused

--- bot/src/announcer.rs
+++ bot/src/announcer.rs
@@ -67,3 +67,91 @@ pub fn update_presence(ctx: &SerenityContext, target: DateTime<Utc>) {
         ctx.set_activity(Some(activity));
     }
 }
+
+#[allow(unused)]
+mod league_start {
+    use crate::{announcer::Offset, channel::AppChannel, emoji::Emoji, SerenityContext};
+    use chrono::{DateTime, NaiveDate, Utc};
+    use futures::future::join_all;
+    use rand::Rng;
+
+    // May 29th, 20:00 UTC
+    const LEAGUE_START: DateTime<Utc> = DateTime::<Utc>::from_naive_utc_and_offset(
+        NaiveDate::from_ymd_opt(2026, 5, 29)
+            .unwrap()
+            .and_hms_opt(20, 0, 0)
+            .unwrap(),
+        Utc,
+    );
+
+    fn presence() {
+        // let presence_updater = async move {
+        //     let mut interval = tokio::time::interval(Duration::from_mins(1));
+        //     loop {
+        //         interval.tick().await;
+        //         announcer::update_presence(ctx, LEAGUE_START);
+        //     }
+        // };
+    }
+
+    #[allow(unused)]
+    async fn league_start_announcer(ctx: &SerenityContext) {
+        fn generate_emojis() -> (String, String) {
+            let mut emojis: Vec<String> = [
+                "⏰", "🚨", "🐸", "🔥", "🎮", "✨", "🎉", "🚀", "🌟", "🔴", "💥", "⚡", "🌈", "😎",
+                "🐺",
+            ]
+            .into_iter()
+            .map(|s| s.to_string())
+            .chain(Emoji::all().into_iter().map(|e| e.to_string()))
+            .collect();
+
+            let mut rng = rand::rng();
+            let mut pick = || emojis.swap_remove(rng.random_range(0..emojis.len()));
+
+            (
+                format!("{}{}{}", pick(), pick(), pick()),
+                format!("{}{}{}", pick(), pick(), pick()),
+            )
+        }
+
+        join_all(
+            (2..20)
+                .map(|d| Offset::Days(d as i64))
+                .chain([
+                    Offset::Hours(30),
+                    Offset::Hours(27),
+                    Offset::Hours(24),
+                    Offset::Hours(20),
+                    Offset::Hours(16),
+                    Offset::Hours(12),
+                    Offset::Hours(10),
+                    Offset::Hours(8),
+                    Offset::Hours(6),
+                    Offset::Hours(5),
+                    Offset::Hours(4),
+                    Offset::Hours(3),
+                    Offset::Hours(2),
+                    Offset::Hours(1),
+                    Offset::Minutes(45),
+                    Offset::Minutes(30),
+                    Offset::Minutes(15),
+                    Offset::Minutes(10),
+                    Offset::Minutes(5),
+                    Offset::Minutes(2),
+                    Offset::Minutes(1),
+                ])
+                .filter(|o| o.is_upcoming(LEAGUE_START))
+                .map(move |offset| async move {
+                    offset
+                        .schedule(LEAGUE_START, move || async move {
+                            let (e1, e2) = generate_emojis();
+                            let msg = format!("{e1} League starts in {}! {e2}", offset.label());
+                            AppChannel::Poe2.say(ctx, &msg).await;
+                        })
+                        .await;
+                }),
+        )
+        .await;
+    }
+}
